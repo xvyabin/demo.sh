9d62497cac06232fffa52685c5000d2bd141606e
xyyabin <792134478@qq.com>
2018-10-29 11:55:40 +0800

    第一次提交

---
+++ jsdemo.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+var fs = require('fs')
+
+var dirName = process.argv[2] // 你传的参数是从第 2 个开始的
+
+if(fs.existsSync(dirName)){
+	console.log(dirName +"该文件已存在")}
+else{
+fs.mkdirSync("./" + dirName) // mkdir $1
+process.chdir("./" + dirName) // cd $1
+fs.mkdirSync('css') // mkdir css
+fs.mkdirSync('js') // mkdir js
+
+fs.writeFileSync("./index.html", "<!DOCTYPE> \n <title>Hello</title> \n<h1>Hi</h1>")
+fs.writeFileSync("css/style.css", "h1{color: red;}")
+fs.writeFileSync("./js/main.js", "var string =\"Hello World\" \nalert(string)")
+
+process.exit(0)}
